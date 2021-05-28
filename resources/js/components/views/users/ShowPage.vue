2d41675e1ca3a76df67ce9418be07d7364f275cb
Mar4elius <marauman@gmail.com>
2021-05-28 10:32:38 -0600

    Creat album tile component

--- resources/js/components/views/users/ShowPage.vue
+++ resources/js/components/views/users/ShowPage.vue
@@ -15,42 +15,7 @@
                         :key="album.id"
                         class="rounded-lg bg-white"
                     >
-                        <div class="relative">
-                            <img
-                                :src="`/${album.image}`"
-                                class="rounded-t-lg"
-                            />
-                            <h6 class="absolute bottom-2 left-2 text-white">
-                                {{ album.title }}
-                            </h6>
-                        </div>
-                        <div class="p-2 md:p-4 lg:p-6">
-                            <p class="text-black">
-                                {{ album.description }}
-                            </p>
-                            <div class="flex justify-between mt-2">
-                                <div>
-                                    <svg
-                                        v-if="album.is_featured"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        class="h-8 w-8"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                                            clip-rule="evenodd"
-                                        />
-                                    </svg>
-                                </div>
-                                <div>
-                                    <p class="text-gray-400">
-                                        {{ album.date }}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                        <album-tile :album="album" />
                     </div>
                 </div>
             </div>
@@ -64,8 +29,10 @@ import { useStore } from "vuex";
 // Components
 import AppLayout from "../../layouts/AppLayout";
 import PhotographerTile from "../../blocks/PhotographerTile";
+import AlbumTile from "../../blocks/AlbumTile";
+
 export default {
-    components: { AppLayout, PhotographerTile },
+    components: { AlbumTile, AppLayout, PhotographerTile },
 
     props: {
         user: {
